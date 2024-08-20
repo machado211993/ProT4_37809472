d5f0189412a0f98468b66f11ad136a8de43e06cd
Manuel <manuel.machado.j.m@gmail.com>
2024-08-20 02:16:07 -0300

    prueba postman

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,6 @@ import { libro } from "./controller.js";
 export const router = Router()
 router.get('/libros', libro.getAll);
 router.post('/libro', libro.add);
-router.delete('/libro', libro.delete);
+router.delete("/libro/:isbn", libro.delete);
 router.put('/libro', libro.update);
 router.get("/libros/:id", libro.getOne);
